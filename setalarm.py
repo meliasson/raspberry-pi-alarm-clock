bae86b518634581f65c6ffeee9080808cf4e6750
Mikael Eliasson <mikael.e.eliasson@gmail.com>
2019-12-30 18:01:28 +0100

    Adjust alarm setting

--- setalarm.py
+++ setalarm.py
@@ -5,16 +5,9 @@ import sys
 hour = sys.argv[1]
 minute = sys.argv[2]
 path = os.getcwd()
+command = "pipenv run python startalarm.py"
 
 subprocess.call("crontab -l > mycron", shell=True)
-subprocess.call(f"echo \"{minute} * * * * cd {path} && pipenv run python startalarm.py\" >> mycron", shell=True)
+subprocess.call(f"echo \"{minute} {hour} * * * cd {path} && {command}\" >> mycron", shell=True)
 subprocess.call("crontab mycron", shell=True)
 subprocess.call("rm mycron", shell=True)
-
-#write out current crontab
-#crontab -l > mycron
-#echo new cron into cron file
-#echo "00 09 * * 1-5 echo hello" >> mycron
-#install new cron file
-#crontab mycron
-#rm mycron
